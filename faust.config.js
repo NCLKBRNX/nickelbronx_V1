cb832ea1e4206f89a83b5ebd1dc2ea5a57ae9e94
kamal <kamal@ifline.com>
2024-09-09 18:09:33 +0530

    Faust Js Config file updated

--- faust.config.js
+++ faust.config.js
@@ -1,6 +1,7 @@
 import { setConfig } from "@faustwp/core";
 import templates from "./src/wp-templates";
 import possibleTypes from "./possibleTypes.json";
+import { InMemoryCache } from '@apollo/client';
 
 /**
  * @type {import('@faustwp/core').FaustConfig}
@@ -9,4 +10,18 @@ export default setConfig({
   templates,
   experimentalPlugins: [],
   possibleTypes,
+
+  // Apollo Client settings
+  apolloClient: {
+    cache: new InMemoryCache(),
+    defaultOptions: {
+      query: {
+        fetchPolicy: 'no-cache', // Disable caching globally
+        errorPolicy: 'all',
+      },
+      mutate: {
+        errorPolicy: 'all',
+      },
+    },
+  },
 });
